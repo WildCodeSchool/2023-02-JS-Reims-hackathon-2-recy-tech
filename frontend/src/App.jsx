8cd43e7c161afcb6b231a56fc97fe55ec0b51bb5
Nassim Cheikh rouhou <nassimcheikhrouhou@gmail.com>
2023-06-28 17:05:21 +0200

    ajout formulaire

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
+import Addphone from "./components/Addphone";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Addphone />
       <Footer />
     </div>
   );
