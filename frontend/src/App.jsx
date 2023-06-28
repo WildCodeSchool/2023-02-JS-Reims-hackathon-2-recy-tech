8848521ddc191410461a8a967de71891b9d841be
Nassim Cheikh rouhou <nassimcheikhrouhou@gmail.com>
2023-06-28 17:12:02 +0200

    merge complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 import "./App.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Home />
+      <Footer />
     </div>
   );
 }
