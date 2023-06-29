aed0fc6ca9da6d297a6a03cc099ec59574b099df
Maachino <maach.med@yahoo.com>
2023-06-29 15:05:12 +0200

    calcul comp

---
+++ frontend/src/services/calcul.js
@@ -0,0 +1,9 @@
+function calcul(ram, storage, antutu) {
+  const price = 0;
+  const category = "test";
+
+  return [price, category];
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { calcul };
